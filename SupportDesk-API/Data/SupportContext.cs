3ba808bd547c24de4b0517ce74ec41a6d6e21f3e
Daniel-Ramos-21 <dcrespin990@gmail.com>
2026-09-01 15:23:13 -0600

    Creación de la base de datos.

---
+++ SupportDesk-API/Data/SupportContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SupportDesk_API.Data
+{
+    public class SupportContext : DbContext
+    {
+        public SupportContext(DbContextOptions<SupportContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
--- SupportDesk-API/Program.cs
+++ SupportDesk-API/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SupportDesk_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<SupportContext>(
+    options => { options.UseSqlServer(builder.Configuration.GetConnectionString("conexion"));});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
