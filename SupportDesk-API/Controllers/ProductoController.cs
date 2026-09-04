c7302da2dd2397d4809d69ea4f7cc4c0fb92d428
Daniel-Ramos-21 <dcrespin990@gmail.com>
2026-09-03 18:21:09 -0600

    Prueba de controlador de la API

---
+++ SupportDesk-API/Controllers/ProductoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SupportDesk_API.Data;
+using SupportDesk_API.Models;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace SupportDesk_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductoController : ControllerBase
+    {
+        private readonly SupportContext _context;
+
+        public ProductoController(SupportContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ejemplo>>> GetAll() => await _context.Ejemplos.ToListAsync();
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Ejemplo>> GetById(int Id)
+        {
+            var producto = await _context.Ejemplos.FindAsync(Id);
+            return producto is null ? NotFound() : producto;
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Ejemplo>> Create(Ejemplo producto)
+        {
+            _context.Ejemplos.Add(producto);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = producto.Id }, producto);
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> Update(int Id, Ejemplo ejemplo)
+        {
+            if (Id != ejemplo.Id) return BadRequest();
+            _context.Entry(ejemplo).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Detele(int id)
+        {
+            var productos = await _context.Ejemplos.FindAsync(id);
+            if (productos is null) return NotFound();
+            _context.Ejemplos.Remove(productos);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    
+    }
+}
---
+++ SupportDesk-API/Migrations/20260904001336_Migración de ejemplo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupportDesk_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracióndeejemplo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ejemplos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ejemplos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ejemplos");
+        }
+    }
+}
